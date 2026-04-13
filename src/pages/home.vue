1484bd12b9d53cdbfe57353e3490c38ad99f59b6
igprog <igprog@yahoo.com>
2026-04-13 15:36:16 +0200

    Small fix and discount.

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -91,7 +91,7 @@
                     <div class="row text-center">
                         <div class="col-md-4 mb-4">
                             <icon name="check-circle" scale=3 class="text-main mb-3" />
-                            <h5 class="font-weight-bold">18 godina iskustva</h5>
+                            <h5 class="font-weight-bold">19 godina razvoja</h5>
                             <p class="feature-text">Stabilan i razvijan u saradnji sa profesionalcima iz prakse.</p>
                         </div>
                         <div class="col-md-4 mb-4">
@@ -130,16 +130,16 @@
                 <div class="container">
                     <div class="row text-center">
                         <div class="col-md-4 mb-4">
-                            <div class="circle-stat mb-3">+740</div>
+                            <div class="circle-stat mb-3">+770</div>
                             <h5 class="text-main font-weight-bold">Aktivnih korisnika</h5>
                         </div>
                         <div class="col-md-4 mb-4">
-                            <div class="circle-stat mb-3">+134.000</div>
+                            <div class="circle-stat mb-3">+170 000</div>
                             <h5 class="text-main font-weight-bold">Kreiranih jelovnika</h5>
                         </div>
                         <div class="col-md-4 mb-4">
-                            <div class="circle-stat mb-3">+18</div>
-                            <h5 class="text-main font-weight-bold">Godina razvoja</h5>
+                            <div class="circle-stat mb-3">+17 000</div>
+                            <h5 class="text-main font-weight-bold">Klijenata</h5>
                         </div>
                     </div>
                 </div>
@@ -224,7 +224,7 @@
                     <h2 class="font-weight-bold text-light mb-3">Započni odmah sa Programom Ishrane</h2>
                     <p class="lead mb-4">
                         Pridruži se stotinama zadovoljnih korisnika širom regiona i isprobaj besplatno! Uveri se zašto nas
-                        nutricionisti, poliklinike i obrazovne ustanove biraju već 18 godina.
+                        nutricionisti, poliklinike i obrazovne ustanove biraju već 19 godina.
                     </p>
                     <a href="https://www.programprehrane.com/registracija?lang=sr"
                         class="btn btn-light m-2 main-border bg-white d-inline-flex align-items-center justify-content-center"
